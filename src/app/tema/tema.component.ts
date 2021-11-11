8ad4019827abb4c62c7ba095a0b123edd7cd7602
Zoraten <cosmos_blood@outlook.com>
2021-11-10 21:59:31 -0300

    [Feature Guilherme - Task: Added in tema GET & POST methods | 10.11.21]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { TemaModel } from '../model/TemaModel';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,8 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: TemaModel = new TemaModel()
+  listaTema: TemaModel[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -18,6 +24,21 @@ export class TemaComponent implements OnInit {
       alert('Sua sessão foi encerrada, faça o login novamente!')
       this.router.navigate(['/entrar'])
     }
+    this.temaService.refreshToken()
+    this.getAllTemas()
+  }
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: TemaModel[]) => {
+      this.listaTema = resp
+    })
   }
 
+  cadastrarTema(){
+      this.temaService.postTema(this.tema).subscribe((resp: TemaModel) => {
+        this.tema = resp
+        alert('O tema foi cadastrado!')
+        this.getAllTemas()
+        this.tema = new TemaModel()
+      })
+  }
 }
